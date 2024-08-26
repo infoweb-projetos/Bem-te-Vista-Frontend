8b1e639abf492eb152d1fea6ec49b7afaf93a342
iraikare <iraikare1810@gmail.com>
2024-08-26 17:54:09 -0300

    mensagens de erro

--- src/componentes/Cadastro/Cadastro.tsx
+++ src/componentes/Cadastro/Cadastro.tsx
@@ -2,104 +2,130 @@ import '../../App.css';
 import React, { useState } from 'react';
 import api from '../../axiosConfig';
 
-import bgforms from '../../imagens/bg-forms.png';
+import bgforms from '../../imagens/bg-signup.png';
 import cabide from '../../imagens/cabide.svg';
 import logo from '../../imagens/logo.svg';
+import olhoAbertoIcon from '../../imagens/Icons/olho-aberto-icon.svg';
+import olhoFechadoIcon from '../../imagens/Icons/olho-fechado-icon.svg';
 
 const UserForm: React.FC = () => {
   const [nome, setName] = useState('');
   const [email, setEmail] = useState('');
   const [nome_de_usuario, setNome_de_usuario] = useState('');
   const [senha, setSenha] = useState('');
+  const [senhaVisivel, setSenhaVisivel] = useState(false);
+  const [senhaErro, setSenhaErro] = useState('');
+
+  const validatePassword = (password: string) => {
+    const regex = /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/;
+    return regex.test(password);
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    api.post('/users', { nome, email, nome_de_usuario, senha })
-      .then(response => {
-        console.log('Usuário criado:', response.data);
-      })
-      .catch(error => {
-        console.error('Erro ao criar usuário:', error);
-      });
+    if (validatePassword(senha)) {
+      api.post('/users', { nome, email, nome_de_usuario, senha })
+        .then(response => {
+          console.log('Usuário criado:', response.data);
+        })
+        .catch(error => {
+          console.error('Erro ao criar usuário:', error);
+        });
+    } else {
+      setSenhaErro('Senha deve ter letras e números.');
+    }
+  };
+
+  const togglePassword = () => {
+    setSenhaVisivel(!senhaVisivel);
   };
 
   return (
     <div className="flex">
-        <div className="bg-white font-[Poppins] flex flex-col items-center justify-center w-4/12 h-[100vh]">
-          <div className="flex flex-col mx-[5rem] mb-[6rem]">
-            <img src={logo} width="250" alt="Logo" />
-            <h2 className="text-2xl mt-[3rem]">
-              <b>Voe</b> no seu estilo
-            </h2>
-            <p className="mt-[1rem] text-lg">
-              Conheça os mundos da moda na nossa plataforma prática e acessível!
-            </p>
+      <div className="bg-[#EDECE7] font-[Martel Sans] flex flex-col items-center justify-center w-4/12 h-[100vh]">
+        <div className="flex flex-col justify-center w-2/4">
+          <img src={logo} width="250" alt="Logo" />
+          <h2 className="text-4xl mt-[3rem]"><b>Voe</b> no seu estilo</h2>
+          <p className="mt-[1rem] text-2xl">Conheça os mundos da moda na nossa plataforma prática e acessível!</p>
+        </div>
+      </div>
+      <main
+        className="bg-cover w-8/12 h-[100vh] flex flex-col items-center justify-center"
+        style={{ backgroundImage: `url(${bgforms})` }}
+      >
+        <img src={cabide} alt="Cabide" />
+        <div
+          className="bg-[rgba(0,0,0,0.6)] border-2 border-white flex flex-col items-center justify-center text-white w-[28rem] py-[2rem]"
+        >
+          <h2 className="font-[Rufina] font-bold text-4xl mb-[1rem]">Cadastre-se!</h2>
+          <form onSubmit={handleSubmit} className="font-[Martel Sans] flex flex-col items-center">
+            <label className="self-start">Nome</label>
+            <input
+              placeholder="Digite aqui..."
+              type="text"
+              className="bg-[rgba(0,0,0,0)] border border-[#EDECE7] w-[18rem] py-[0.4rem] px-[0.4rem] mt-[0.3rem]"
+              value={nome}
+              onChange={(e) => setName(e.target.value)}
+              required
+              minLength={4}
+            />
+            <label className="mt-[0.8rem] self-start">Username</label>
+            <input
+              placeholder="Digite aqui..."
+              type="text"
+              className="bg-[rgba(0,0,0,0)] border border-[#EDECE7] w-[18rem] py-[0.4rem] px-[0.4rem] mt-[0.3rem]"
+              value={nome_de_usuario}
+              onChange={(e) => setNome_de_usuario(e.target.value)}
+              required
+              minLength={4}
+            />
+            <label className="mt-[0.8rem] self-start">E-mail</label>
+            <input
+              placeholder="seuemail@gmail.com..."
+              type="email"
+              className="bg-[rgba(0,0,0,0)] border border-[#EDECE7] w-[18rem] py-[0.4rem] px-[0.4rem] mt-[0.3rem]"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label className="mt-[0.8rem] self-start">Senha</label>
+            <div className="flex flex-col items-center">
+              <div className="flex justify-between border border-[#EDECE7] w-[18rem] py-[0.4rem] px-[0.4rem] mt-[0.3rem]">
+                <input
+                  placeholder="@Senha123!..."
+                  type={senhaVisivel ? "text" : "password"}
+                  className="bg-[rgba(0,0,0,0)] w-[90%]"
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                  required
+                  minLength={8}
+                />
+                <img
+                  src={senhaVisivel ? olhoFechadoIcon : olhoAbertoIcon}
+                  id="olhoicon"
+                  onClick={togglePassword}
+                  alt="Toggle Password Visibility"
+                />
+              </div>
+              <div className="w-[18rem] mt-2 "> {/* Contêiner com altura fixa para a mensagem de erro */}
+                {senhaErro && <p className="text-red-500">{senhaErro}</p>}
+              </div>
+            </div>
+            <button type="submit" className="bg-[#F9C62E] self-center text-black w-[8rem] py-[0.3rem] mt-[1.5rem] hover:cursor-pointer hover:bg-[#EDECE7] transition duration-300 ease-in-out hover:border-[#EDECE7] hover:text-black">
+              Criar
+            </button>
+          </form>
+          <div className="flex flex-col items-center mt-[1rem]">
+            <div className="flex items-center">
+              <hr className="w-[3rem] border border-[#EDECE7]" />
+              <p className="mx-[0.4rem] mt-[-0.2rem] text-xs">ou</p>
+              <hr className="w-[3rem] border border-[#EDECE7]" />
+            </div>
+            <p className="mt-[0.5rem] text-xs">Já tem uma conta? <a href="/Login" className="font-medium hover:underline">Conecte-se</a></p>
           </div>
         </div>
-        <main
-          className="bg-cover w-8/12 h-[100vh] flex flex-col items-center justify-center"
-          style={{ backgroundImage: `url(${bgforms})` }}
-        >
-          <img src={cabide} alt="Cabide" />
-          <div className="bg-[rgba(0,0,0,0.6)] border-2 border-white flex flex-col items-center justify-center text-white w-[30rem] h-[36rem] py-[2rem]">
-            <h2 className="font-[Rufina] font-bold text-4xl mb-[2rem]">Cadastre-se</h2>
-    <form onSubmit={handleSubmit} className="font-[Martel Sans] flex flex-col items-center">
-
-      <label>Nome</label>
-      <input
-        type="text"
-        className="bg-[rgba(0,0,0,0.6)] border border-white w-[18rem] py-[0.2rem] px-[0.4rem] mt-[0.3rem]"
-        value={nome}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Digite aqui..."
-      />
-
-      <label className="mt-[1rem]">E-mail</label>
-      <input
-        type="email"
-        className="bg-[rgba(0,0,0,0.6)] border border-white w-[18rem] py-[0.2rem] px-[0.4rem] mt-[0.3rem]"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Digite aqui..."
-      />
-
-      <label className="mt-[1rem]">Nome de Usuário</label>
-      <input
-        type="nome_de_usuario"
-        className="bg-[rgba(0,0,0,0.6)] border border-white w-[18rem] py-[0.2rem] px-[0.4rem] mt-[0.3rem]"
-        value={nome_de_usuario}
-        onChange={(e) => setNome_de_usuario(e.target.value)}
-        placeholder="Digite aqui..."
-      />
-
-      <label className="mt-[1rem]">Senha</label>
-      <input
-        type="senha"
-        className="bg-[rgba(0,0,0,0.6)] border border-white w-[18rem] py-[0.2rem] px-[0.4rem] mt-[0.3rem]"
-        value={senha}
-        onChange={(e) => setSenha(e.target.value)}
-        placeholder="Digite aqui..."
-      />
-      <button type="submit" className="bg-[rgba(0, 0, 0, 0.6)] border border-white px-[2rem] py-[0.3rem] mt-[2rem] hover:cursor-pointer hover:bg-[#F9C62E] hover:border-[#F9C62E] hover:text-black">Criar Usuário</button>
-    </form>
-
-    <div className="flex flex-col items-center mt-[0.5rem]">
-              <div className="flex items-center">
-                <hr className="w-[4rem] border border-white" />
-                <p className="mx-[0.3rem]">ou</p>
-                <hr className="w-[4rem] border border-white" />
-              </div>
-              <p className="mt-[0.5rem] text-sm">
-                Já possui uma conta?{" "}
-                <a href="/Login" className="hover:underline">
-                  Conecte-se
-                </a>
-              </p>
-            </div>
-
-      </div>
-    </main>
-  </div>
+      </main>
+    </div>
   );
 };
 
--- src/componentes/Login/Login.tsx
+++ src/componentes/Login/Login.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import api from '../../axiosConfig';
-import '../../App.css'
 
-import logo from '../../imagens/logo.svg'
-import cabide from '../../imagens/cabide.svg'
-import bgforms from '../../imagens/bg-forms.png'
+import logo from '../../imagens/logo.svg';
+import cabide from '../../imagens/cabide.svg';
+import bgforms from '../../imagens/bg-login.png';
+import olhoAbertoIcon from '../../imagens/Icons/olho-aberto-icon.svg';
+import olhoFechadoIcon from '../../imagens/Icons/olho-fechado-icon.svg';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [mostrarSenha, setMostrarSenha] = useState(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -16,7 +18,6 @@ const Login = () => {
     api.post('/auth/login', { email, senha })
       .then(response => {
         console.log('Logged in:', response.data);
-        // Redirecionar para o feed
         localStorage.setItem('token', response.data.access_token);
       })
       .catch(error => {
@@ -31,64 +32,90 @@ const Login = () => {
       });
   };
 
+  const togglePassword = () => {
+    setMostrarSenha(!mostrarSenha);
+  };
+
   return (
-    <div className="flex">
-    <div className="bg-white font-[Poppins] flex flex-col items-center justify-center w-4/12 h-[100vh]">
-      <div className="flex flex-col mx-[5rem] mb-[6rem]">
-        <img src={logo} width="250" alt="Logo" />
-        <h2 className="text-2xl mt-[3rem]">
-          <b>Voe</b> no seu estilo
-        </h2>
-        <p className="mt-[1rem] text-lg">
-          Conheça os mundos da moda na nossa plataforma prática e acessível!
-        </p>
-      </div>
-    </div>
-    <main className="bg-cover w-8/12 h-[100vh] flex flex-col items-center justify-center"
-    style={{ backgroundImage: `url(${bgforms})` }}>
-        <img src={cabide} alt="Cabide" />
-      <div className="bg-[rgba(0,0,0,0.6)] border-2 border-white flex flex-col items-center justify-center text-white w-[28rem] h-[30rem] py-[2rem]">
-        <h2 className="font-[Rufina] font-bold text-4xl mb-[2rem]">Bem-vindo!</h2>
-    <form onSubmit={handleSubmit} className="font-[Martel Sans] flex flex-col items-center">
-    <label>Email</label>    
-      <input
-        type="text"
-        className="bg-[rgba(0,0,0,0.6)] border border-white w-[18rem] py-[0.2rem] px-[0.4rem] mt-[0.3rem]"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Digite aqui.."
-      />
-      <label className="mt-[2rem]">Senha</label>
-      <div className="flex flex-col items-start">
-      <input
-        type="password"
-        className="bg-[rgba(0,0,0,0.6)] border border-white w-[18rem] py-[0.2rem] px-[0.4rem] mt-[0.3rem]"
-        value={senha}
-        onChange={(e) => setSenha(e.target.value)}
-        placeholder="Digite aqui..."
-      />
-      <a href="#" className="hover:underline mt-[0.5rem] text-sm">
-                  Esqueceu a senha?
-                </a>
-                </div>
-      <button type="submit"  className="bg-[rgba(0, 0, 0, 0.6)] border border-white px-[2rem] py-[0.3rem] mt-[2rem] hover:cursor-pointer hover:bg-[#F9C62E] hover:border-[#F9C62E] hover:text-black">Login</button>
-    </form>
-    <div className="flex flex-col items-center mt-[1rem]">
-              <div className="flex items-center">
-                <hr className="w-[4rem] border border-white" />
-                <p className="mx-[0.3rem]">ou</p>
-                <hr className="w-[4rem] border border-white" />
-              </div>
-              <p className="mt-[1rem] text-sm">
-                Não tem uma conta? <a href="/Cadastro" className="hover:underline">Cadastre-se</a>
-              </p>
-            </div>
-
+    <div className="flex justify-between">
+      <div className="static h-screen bg-[#EDECE7] font-[Martel Sans] flex flex-col items-center justify-center w-4/12">
+        <div className="flex flex-col justify-center w-2/4">
+          <img src={logo} width="250" alt="Logo" />
+          <h2 className="text-4xl mt-[3rem]">
+            <b>Voe</b> no seu estilo
+          </h2>
+          <p className="mt-[1rem] text-2xl">
+            Conheça os mundos da moda na nossa plataforma prática e acessível!
+          </p>
         </div>
-    </main>
-
+      </div>
+      <main
+        className="bg-cover w-8/12 max-md:h-full flex flex-col items-center justify-center h-[100vh] max-lg:h-[125vh]"
+        style={{ backgroundImage: `url(${bgforms})` }}
+      >
+        <img src={cabide} className="mb-[-0.1rem]" alt="Cabide" />
+        <div className="bg-[rgba(0,0,0,0.6)] border-2 border-[#EDECE7] flex flex-col items-center justify-center text-[#EDECE7] w-[28rem] py-[2rem] max-xl:h-[25rem] max-md:h-[15rem]">
+          <h2 className="font-[Rufina] font-bold text-4xl mb-[2rem] max-xl:text-3xl max-lg:text-2xl max-xl:mb-[0.5rem]">
+            Bem-vindo!
+          </h2>
+          <form onSubmit={handleSubmit} className="font-[Martel Sans] flex flex-col items-start">
+            <label>E-mail</label>
+            <input
+              type="text"
+              className="bg-[rgba(0,0,0,0)] border border-[#EDECE7] w-[18rem] py-[0.4rem] px-[0.4rem] mt-[0.3rem] max-lg:h-[1.5rem] max-lg:text-xs"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="seuemail@gmail.com..."
+              required
+            />
+            <label className="mt-[0.8rem] max-xl:mt-[0.5rem]">Senha</label>
+            <div className="flex flex-col items-start">
+              <div className="flex justify-between border border-[#EDECE7] w-[18rem] py-[0.4rem] px-[0.4rem] mt-[0.3rem] max-lg:h-[1.5rem] max-lg:text-xs">
+                <input
+                  type={mostrarSenha ? 'text' : 'password'}
+                  id="senha"
+                  className="bg-[rgba(0,0,0,0)] w-[90%]"
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                  placeholder="@Senha123!..."
+                  required
+                  
+                />
+                <img
+                  src={mostrarSenha ? olhoFechadoIcon : olhoAbertoIcon}
+                  alt="Mostrar senha"
+                  onClick={togglePassword}
+                  className="cursor-pointer"
+                  id="olhoicon"
+                />
+              </div>
+              <a href="#" className="font-medium hover:underline mt-[0.5rem] text-xs">
+                Esqueceu a senha?
+              </a>
+            </div>
+            <button
+              type="submit"
+              className="bg-[#F9C62E] self-center text-black w-[8rem] py-[0.3rem] mt-[1.5rem] hover:cursor-pointer hover:bg-[#EDECE7] transition duration-300 ease-in-out hover:border-[#EDECE7] hover:text-black"
+            >
+              Entrar
+            </button>
+          </form>
+          <div className="flex flex-col items-center mt-[1rem]">
+            <div className="flex items-center">
+              <hr className="w-[3rem] border border-[#EDECE7]" />
+              <p className="mx-[0.4rem] mt-[-0.2rem] text-xs">ou</p>
+              <hr className="w-[3rem] border border-[#EDECE7]" />
+            </div>
+            <p className="mt-[0.5rem] text-xs">
+              Não tem uma conta?{' '}
+              <a href="/Cadastro" className="font-medium hover:underline">
+                Cadastre-se
+              </a>
+            </p>
+          </div>
+        </div>
+      </main>
     </div>
-    
   );
 };
 
